4be135c793c9586b761c33a10eac31567ca428d6
hanna-khan <hanna.khan205@gmal.com>
2025-01-21 20:53:08 +0500

    welcome section

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Welcome from './components/Welcome';
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Welcome />
     </>
   );
 }
