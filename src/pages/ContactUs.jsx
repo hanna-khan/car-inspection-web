60c1a318126ccb4b423bdde42e3af12fc4e5ce86
hanna-khan <hanna.khan205@gmal.com>
2025-02-02 20:58:27 +0500

    contact done

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -105,15 +105,16 @@ const ContactUs = () => {
           <h1 className="text-balance text-3xl my-4 font-semibold tracking-tight text-black">
             Location
           </h1>
-          {/* <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3542.376345655585!2d67.0841779150091!3d24.860963984019877!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3eb33f846f58a5b1%3A0xc772ce68787fbb07!2sKarachi%2C%20Pakistan!5e0!3m2!1sen!2sus!4v1674824733307!5m2!1sen!2sus"
-            width="600"
-            height="450"
-            style="border:0;"
-            allowfullscreen=""
-            loading="lazy"
-            referrerpolicy="no-referrer-when-downgrade">
-          </iframe> */}
+          <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.521260322283!2d106.8195613507864!3d-6.194741395493371!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f5390917b759%3A0x6b45e67356080477!2sPT%20Kulkul%20Teknologi%20Internasional!5e0!3m2!1sen!2sid!4v1601138221085!5m2!1sen!2sid"
+              width="100%"
+              height="450"
+              frameBorder="0"
+              style={{ border: 0 }}
+              allowFullScreen=""
+              aria-hidden="false"
+              tabIndex="0"
+            />
 
         </div>
       </div>
